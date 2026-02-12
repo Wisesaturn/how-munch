a8222c9f153ffd14e4ae86924b035586c7391d45
Wisesaturn <rfv1479@gmail.com>
2026-02-13 02:15:29 +0900

    fix: 로그인 리다이렉트 origin 주소 vercel 배포 기준으로 수정

--- src/features/auth/api/login.ts
+++ src/features/auth/api/login.ts
@@ -1,14 +1,14 @@
 'use server';
 
-import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 
 import { createClient } from '@/commons/api/supabase/server';
 
 export async function loginWithKakao() {
   const supabase = await createClient();
-  const headerStore = await headers();
-  const origin = headerStore.get('origin') ?? 'http://localhost:3000';
+  const origin = process.env.VERCEL_URL
+    ? `https://${process.env.VERCEL_URL}`
+    : 'http://localhost:3000';
 
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: 'kakao',
