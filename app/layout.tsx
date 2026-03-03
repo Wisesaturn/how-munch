f6df2ce9dc8c45a5021dfe2adb49b94c74f593f8
Wisesaturn <rfv1479@gmail.com>
2026-03-03 23:53:33 +0900

    feat: IOS PWA Meta Tag 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import './globals.css';
 import { Providers } from './providers';
 
 export const metadata: Metadata = {
-  title: 'How Munch',
-  description: 'How Munch',
+  title: '얼마먹었니',
+  description: '장보기, 냉장고, 식단 올인원 관리 서비스',
   icons: {
     icon: [
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
@@ -14,6 +14,133 @@ export const metadata: Metadata = {
     apple: '/apple-touch-icon.png',
   },
   manifest: '/site.webmanifest',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: '얼마먹었니',
+    startupImage: [
+      // iPhone SE (1st gen)
+      {
+        url: '/splash/apple-splash-640-1136.png',
+        media:
+          '(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      // iPhone SE (2/3rd gen), 8, 7, 6
+      {
+        url: '/splash/apple-splash-750-1334.png',
+        media:
+          '(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      // iPhone XR, 11
+      {
+        url: '/splash/apple-splash-828-1792.png',
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      // iPhone X, XS, 11 Pro
+      {
+        url: '/splash/apple-splash-1125-2436.png',
+        media:
+          '(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      // iPhone 12, 12 Pro, 13, 13 Pro, 14
+      {
+        url: '/splash/apple-splash-1170-2532.png',
+        media:
+          '(device-width: 390px) and (device-height: 844px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      // iPhone 14 Pro, 15, 15 Pro, 16
+      {
+        url: '/splash/apple-splash-1179-2556.png',
+        media:
+          '(device-width: 393px) and (device-height: 852px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      // iPhone 16 Pro
+      {
+        url: '/splash/apple-splash-1206-2622.png',
+        media:
+          '(device-width: 402px) and (device-height: 874px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      // iPhone 6 Plus, 7 Plus, 8 Plus
+      {
+        url: '/splash/apple-splash-1242-2208.png',
+        media:
+          '(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      // iPhone XS Max, 11 Pro Max
+      {
+        url: '/splash/apple-splash-1242-2688.png',
+        media:
+          '(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      // iPhone 12 Pro Max, 13 Pro Max, 14 Plus
+      {
+        url: '/splash/apple-splash-1284-2778.png',
+        media:
+          '(device-width: 428px) and (device-height: 926px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      // iPhone 14 Pro Max, 15 Plus, 15 Pro Max, 16 Plus
+      {
+        url: '/splash/apple-splash-1290-2796.png',
+        media:
+          '(device-width: 430px) and (device-height: 932px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      // iPhone 16 Pro Max
+      {
+        url: '/splash/apple-splash-1320-2868.png',
+        media:
+          '(device-width: 440px) and (device-height: 956px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)',
+      },
+      // iPad mini (6th gen)
+      {
+        url: '/splash/apple-splash-1488-2266.png',
+        media:
+          '(device-width: 744px) and (device-height: 1133px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      // iPad (9th gen)
+      {
+        url: '/splash/apple-splash-1536-2048.png',
+        media:
+          '(device-width: 768px) and (device-height: 1024px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      // iPad Air (3rd gen), iPad Pro 10.5" 이전
+      {
+        url: '/splash/apple-splash-1620-2160.png',
+        media:
+          '(device-width: 810px) and (device-height: 1080px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      // iPad Air (4/5th gen), iPad (10th gen)
+      {
+        url: '/splash/apple-splash-1640-2360.png',
+        media:
+          '(device-width: 820px) and (device-height: 1180px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      // iPad Air (3rd gen), iPad Pro 10.5"
+      {
+        url: '/splash/apple-splash-1668-2224.png',
+        media:
+          '(device-width: 834px) and (device-height: 1112px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      // iPad Pro 11"
+      {
+        url: '/splash/apple-splash-1668-2388.png',
+        media:
+          '(device-width: 834px) and (device-height: 1194px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      // iPad Pro 12.9"
+      {
+        url: '/splash/apple-splash-2048-2732.png',
+        media:
+          '(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+      // iPad mini (7th gen)
+      {
+        url: '/splash/apple-splash-1260-2736.png',
+        media:
+          '(device-width: 630px) and (device-height: 1368px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)',
+      },
+    ],
+  },
 };
 
 export const viewport: Viewport = {
